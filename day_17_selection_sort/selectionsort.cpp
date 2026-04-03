f55558df7cdd662f3b74d6d78b5350e00af87a32
aadiwaykole <adityawaykole494@gmail.com>
2026-04-03 23:55:52 +0530

    code done of selection sorting

--- day_17_selection_sort/selectionsort.cpp
+++ day_17_selection_sort/selectionsort.cpp
@@ -4,5 +4,22 @@ using namespace std;
 
 int main (){
 
-    int 
-}
+    int arr[6] = { 64, 25, 12, 22, 11, 10};
+
+    for (int i= 0; i<5; i++ ){
+
+        int index = i ; 
+
+        for (int j = i+1; i<6; j++){
+
+            if(arr[j]<arr[index])
+            index = j;
+        }
+
+        swap (arr[i], arr[index]);
+    }
+
+    for (int i = 0; i<6; i++){
+        cout << arr[i] << " ";
+    }; 
+};
