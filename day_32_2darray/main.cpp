198adc39fde6c7d100ba36a2bb81b3fe0e0dd099
Aditya Waykole <adityawaykole494@gmail.com>
2026-05-30 18:01:12 +0530

    c

--- day_32_2darray/main.cpp
+++ day_32_2darray/main.cpp
@@ -4,14 +4,36 @@
 
 using namespace std; 
 
+void wave(int arr[][4], int row, int col )
+{
+    for(int j=0; j<col; j++){
+
+        //
+        if(j%2==0)
+        {
+            for(int i=0; i<row; i++)
+            {
+                cout<< arr[i][j]<<" ";
+            }
+        }
+        else
+        {
+            for(int i=row-1; i>=0;i--)
+            {
+                cout<< arr[i][j]<< " ";
+            }
+        }
+    }
+}
+
 int main(){
 
     //create 2d vector
 
-    int n, m ;
-    cout<<"enter rows and column ";
-    cin>>n>>m ; 
-    vector<vector<int>>matrix(n,vector<int>(m,1));
+    // int n, m ;
+    // cout<<"enter rows and column ";
+    // cin>>n>>m ; 
+    // vector<vector<int>>matrix(n,vector<int>(m,1));
 
     
     // for(int i =0; i<3; i++){
@@ -32,20 +54,24 @@ int main(){
     // cout<<endl;
     // cout<<"col = " <<matrix[0].size();
 
-    for(int i=0; i<n ; i++){
-        for(int j=0; j<m; i++){
-            cin>> matrix[i][j];
+    // for(int i=0; i<n ; i++){
+    //     for(int j=0; j<m; i++){
+    //         cin>> matrix[i][j];
 
-        }
-    }
+    //     }
+    // }
 
-     for(int i=0; i<n ; i++){
-        for(int j=0; j<m; i++){
-            cout<< matrix[i][j];
-            cout<< endl; 
+    //  for(int i=0; i<n ; i++){
+    //     for(int j=0; j<m; i++){
+    //         cout<< matrix[i][j];
+    //         cout<< endl; 
 
-        }
-    }
+    //     }
+    // }
 
+    //wave print 
 
+    int arr [3][4]= {3,45,6,7,3,456,7,8,6,4,2,5};
+
+     wave(arr, 3,4);
 }
