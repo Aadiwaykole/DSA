9379073e87e07dbc431bb84a0700be5d4450e4f0
Aditya Waykole <adityawaykole494@gmail.com>
2026-06-15 22:53:40 +0530

    string started

---
+++ day_36/string.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+
+    // char arr[]= {'a','p','p','l'};
+
+    // for (int i=0; i<5; i++)
+    // cout<< arr[i] << " ";
+
+
+    // char arr[20];
+    // cin>> arr;
+    // arr[2]= '\0';
+    // cout<<arr;
+
+
+    //string
+
+    // string s = "aditya";
+    // cout << "your string is= "<< s ; 
+
+    //user input 
+    // string s ; 
+    // cin>> s; 
+    // cout<<s; 
+    //IF YOU WANT TO SPALCE IN YOUR OUTPUT USE GETLINE
+    // string s;
+    // getline(cin, s );
+    // cout <<s <<endl;
+    // cout<<s.size()<<endl; 
+
+
+//append operator 
+    // string a ="aditya";
+    // string b ="waykole ";
+
+    // string s2 = a.append(b);
+    // cout << s2<<endl;
+    // cout<<endl ; 
+
+    // string c = a+b; 
+
+    // a.push_back ('b');
+    // cout<< a << endl ;
+
+    // cout<< c;
+
+
+    // string m = "rohit is a "good" boy"'
+    //for printing cotation use escape character
+    // string s = "aditya is "good" boy";
+    //gives error
+
+    string s = "aditya is \"good\" boy";
+    cout << s << endl; 
+
+}
