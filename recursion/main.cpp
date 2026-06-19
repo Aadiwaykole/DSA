c001810befb937c40600af8c7ca37c60883e5326
Aditya Waykole <adityawaykole494@gmail.com>
2026-06-19 08:52:11 +0530

    updated

--- recursion/main.cpp
+++ recursion/main.cpp
@@ -3,24 +3,58 @@
 
 using namespace std; 
 
-void fun3(int n ){
-    cout << n << "days left for birthday"<< endl ;
+// void fun0 (int n ){
+//     cout  << "days left for birthday"<< endl;
+// }
 
-}
+// void fun1 (int n ){
+//     cout << n << "days left for birthday"<< endl;
+//     fun0(0);
+// }
 
-void fun2 (int n ){
-    cout << n << "days left for birthday"<< endl;
-}
+// void fun2 (int n ){
+//     cout << n << "days left for birthday"<< endl;
+//     fun1(n-1);
+    
+// }
 
-void fun1 (int n ){
-    cout << n << "days left for birthday"<< endl;
-}
-
-void fun0 (int n ){
-    cout  << "days left for birthday"<< endl;
-}
+// void fun3(int n ){
+//     cout << n << "days left for birthday"<< endl ;
+//     fun2(n-1);
+// }
 
 
+// void fun3(int n ){
+//     cout << n << "days left for birthday"<< endl ;
+
+// }
+
+// void fun2 (int n ){
+//     cout << n << "days left for birthday"<< endl;
+// }
+
+// void fun1 (int n ){
+//     cout << n << "days left for birthday"<< endl;
+// }
+
+// void fun0 (int n ){
+//     cout  << "days left for birthday"<< endl;
+// }
+
+//more simple way to define i t
+
+void fun3( int n ){
+
+    //base case 
+
+    if(n==0){
+        cout << "happy birthday "<< endl; 
+        return ; 
+    }
+    cout << n << "days left for birthday"<< endl ; 
+    fun3(n-1);
+
+};
 int main (){
 
     // int n =5;
@@ -37,10 +71,13 @@ int main (){
 
     int n = 3; 
 
+    // fun3(3);
+    // fun2(2);
+    // fun1(1); 
+    // fun0(0);
+
+
     fun3(3);
-    fun2(2);
-    fun1(1); 
-    fun0(0);
 
 
 }
