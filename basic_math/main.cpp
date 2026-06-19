5d9a8ca18f7bb0b8aedd6fc93639b58b51519ac7
Aditya Waykole <adityawaykole494@gmail.com>
2026-06-19 08:36:32 +0530

    recursion

---
+++ basic_math/main.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+using namespace std; 
+
+int main(){
+
+    
+    int num = 7789; 
+
+    while (num > 0 ){
+
+        int lastdigit = num %10; 
+
+        num = num /10; 
+
+
+    }
+
+    
+}
